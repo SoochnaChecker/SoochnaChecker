cf48293f3147cae5219361e42e7a6b9cbd0d3d37
Om Sarraf <itsomsarraf+github@gmail.com>
2023-04-21 15:15:56 +0530

    Delete chrome-extension directory

--- chrome-extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("open-iframe").addEventListener("click", function () {
-  var inputUrl = document.getElementById("input").value;
-  var iframeUrl =
-    chrome.runtime.getURL("iframe.html") +
-    "?url=" +
-    encodeURIComponent(inputUrl);
-  chrome.tabs.create({ url: iframeUrl });
-  console.log(iframeUrl);
-});
